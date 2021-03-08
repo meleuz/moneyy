732786107cc8659daadeef052f0f6f5fc705b18f
Aigul Biktasheva <meleuz13@gmail.com>
2021-03-08 12:22:57 +0500

    balance added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args)  {
+        int balance = 2_000_000_000;
+        int transfer_to = 500_000_000;
+        int new_balance = balance + transfer_to;
+        System.out.println(new_balance);
+    }
+}
